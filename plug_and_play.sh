2cda6dc63ef289e1c12a949c3c56dbbdbd0c2ee2
Eduardo Gutierrez Maestro <eduardo.gutierrez.maestro.1994@gmail.com>
2018-12-13 13:02:14 +0100

    bug fixed

--- plug_and_play.sh
+++ plug_and_play.sh
@@ -6,7 +6,7 @@
 
 sudo service mongod start #Inicia el servicio de mongDB
 sudo iptables -A INPUT -p tcp --dport 80 -j ACCEPT  #abre el puerto 80
-python ~/flaskAppCompu/run.py  #ejecuta el script de la aplicacion
+sudo python ~/flaskAppCompu/run.py  #ejecuta el script de la aplicacion
 
 
 
