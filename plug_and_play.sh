14b5dcc23a0b019f7820fa6e4e1cf7027e340e73
Eduardo Gutierrez Maestro <eduardo.gutierrez.maestro.1994@gmail.com>
2018-12-13 12:27:08 +0100

    plug_and_play.sh bug fixed

--- plug_and_play.sh
+++ plug_and_play.sh
@@ -4,7 +4,7 @@
 #Este script crea un demonio para que se ejecute cada vez que se inicie la maquina
 
 
-sudo servide mongod start #Inicia el servicio de mongDB
+sudo service mongod start #Inicia el servicio de mongDB
 sudo iptables -A INPUT -p tcp --dport 80 -j ACCEPT  #abre el puerto 80
 python ~/flaskAppCompu/run.py  #ejecuta el script de la aplicacion
 
